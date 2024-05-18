16c69a7c3757218741d78e8971b7c4bdb17829cb
Chase Odom <chasemichaelodom@gmail.com>
2024-05-17 21:49:22 -0700

    trying some things

--- Animated Button/animated_button.gd
+++ Animated Button/animated_button.gd
@@ -1,7 +1,28 @@
+@tool
+
 extends Control
 
-@export var anim_player : AnimationPlayer
-@export var button_color : Color
+@onready var anim_player : AnimationPlayer = $AnimationPlayer
+@onready var top : Panel = $Control/Control/Top
+@onready var text_box : RichTextLabel = $Control/Control/Top/RichTextLabel
+
+@export_multiline var button_text : String = "Animated Button":
+	get:
+		return button_text
+	set(value):
+		await ready
+		push_warning("text!", name, value)
+		text_box.text = value
+		propagate_call("update")
+		button_text = value
+
+@export var button_color : Color = Color(0.66796875, 0, 0):
+	get:
+		return button_color
+	set(value):
+		print("color!", name, value)
+		propagate_call("update")
+		button_color = value
 
 var disabled : bool = false
 
@@ -36,7 +57,6 @@ func gui_input_func(ev : InputEvent):
 				else:
 					play_anim("released")
 					pressed.emit()
-					
 
 func play_anim(anim_name : String):
 	print(anim_name)
--- Assets/main.gd
+++ Assets/main.gd
@@ -3,5 +3,6 @@ extends Node2D
 @export var fancy_button : PackedScene
 
 func _ready():
+	return 0
 	for i in range(20):
 		$GridContainer.add_child(fancy_button.instantiate())
